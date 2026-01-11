1cf7fa0d67de5425cae8411794be802fc2643914
Ana-Daria ZĂHĂLEANU (170809) <ana_daria.zahaleanu@fep8.grid.pub.ro>
2026-01-12 00:55:52 +0200

    added input files

--- slurm_scripts/amoc-run.sh
+++ slurm_scripts/amoc-run.sh
@@ -3,7 +3,7 @@ set -e
 
 SIF_IMAGE="/export/projects/nlp/containers/daria-vllm.sif"
 PROJECT_ROOT="/export/home/acs/stud/a/ana_daria.zahaleanu/to_transfer/amoc-v4-persona-age-experiments"
-INPUT_DIR="${PROJECT_ROOT}/balanced_dfs/"
+INPUT_DIR="${PROJECT_ROOT}/personas_dfs/personas_refined_age"
 OUTPUT_DIR="/export/home/acs/stud/a/ana_daria.zahaleanu/to_transfer/output"
 
 mkdir -p "${OUTPUT_DIR}/extracted_triplets"
