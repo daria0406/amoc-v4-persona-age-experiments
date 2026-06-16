0310a127fa7301590405066554f6bc96c0f3fdad
Daria Zahaleanu <dariazahaleanu@Darias-MacBook-Pro.local>
2026-06-16 10:45:53 +0200

    increased number of personas

--- slurm_scripts/run_llama70b_4gpu_small_example.sh
+++ slurm_scripts/run_llama70b_4gpu_small_example.sh
@@ -62,7 +62,7 @@ fi
 bash "${PROJECT_ROOT}/slurm_scripts/amoc-run.sh" \
     --models "meta-llama/Llama-3.3-70B-Instruct" \
     --tp 4 \
-    --max-rows 10 \
+    --max-rows 20 \
     --plot-after-each-sentence \
     --output-dir "${RUN_OUTPUT_DIR}" \
     --file "${INPUT_FILE}" \
